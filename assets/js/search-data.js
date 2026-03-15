9b73e81d742387320c1ddc59495ebb5e736290b4
inui-kim <117260742+inui-kim@users.noreply.github.com>
2026-03-15 06:43:41 +0000

    Deploying to gh-pages from @ inui-kim/inui-kim.github.io@4a796a333de0b950696d7df94464ee719d7314ac 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -406,8 +406,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-started-teaching-global-issues-and-ethical-thinking-and-studies-in-unification-issues-at-seoul-national-university-smile",
+          title: 'Started teaching Global Issues and Ethical Thinking and Studies in Unification Issues at...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
